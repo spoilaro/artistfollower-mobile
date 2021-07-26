77a679325a346684c2fbab77df4af3f8f980c098
Lionbull <geo1529@yahoo.com>
2021-07-26 20:53:59 +0300

    Artist component

---
+++ components/ArtistButton.js
@@ -0,0 +1,63 @@
+import React from "react";
+import FONTS from "../constants/fonts";
+
+const imHuman =
+  "https://cdn.icon-icons.com/icons2/916/PNG/512/Human_icon-icons.com_71855.png";
+const imPlus =
+  "https://cdn2.iconfinder.com/data/icons/ios-7-icons/50/plus-512.png";
+
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  Button,
+  TouchableOpacity,
+} from "react-native";
+
+export default function ArtistButton({ artistData }) {
+  return (
+    <View style={styles.container}>
+      <View style={{ flexDirection: "row", alignItems: "center", flex: 1 }}>
+        <Image style={styles.image} source={{ uri: imHuman }} />
+        <View>
+          <Text
+            style={{ fontSize: FONTS.size.SEMI_SMALL, marginLeft: 10 }}
+            className="artist-name"
+          >
+            Artist
+          </Text>
+        </View>
+      </View>
+      <View style={{ marginRight: 10 }}>
+        <TouchableOpacity>
+          <Image style={styles.addimage} source={{ uri: imPlus }} />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    borderWidth: 1,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+    alignItems: "center",
+  },
+  image: {
+    margin: 5,
+    width: 30,
+    height: 30,
+    backgroundColor: "white",
+  },
+  addimage: {
+    width: 25,
+    height: 25,
+  },
+  button: {
+    flex: 1,
+    marginLeft: "80%",
+  },
+});
