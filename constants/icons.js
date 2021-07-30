565366e7331c5c4d1fc252a9a4dfd5c564b09bb1
Johannes <johannes.haapanen@gmail.com>
2021-07-30 07:46:52 +0300

    Added some icons to assets

---
+++ constants/icons.js
@@ -0,0 +1,4 @@
+const home = require("../assets/home.png")
+const info = require("../assets/info.png")
+
+export default { home, info }
--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,5 @@
 import colors from "./colors"
 import fonts from "./fonts"
+import icons from "./icons"
 
-export { colors, fonts }
+export { colors, fonts, icons }
