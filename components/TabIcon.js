091780055ace73d8f2c0ddb19b2ddb65ec19fa48
spoilaro <johannes.haapanen@gmail.com>
2021-08-01 09:24:49 +0300

    Added place holder icons to tab bar

--- components/TabIcon.js
+++ components/TabIcon.js
@@ -1,10 +1,11 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from "react";
+import { View, Text, Image } from "react-native";
 
 export default function TabIcon({ focused, icon, label }) {
-    return (
-        <View>
-            <Text>Tab</Text>
-        </View>
-    )
+  return (
+    <View style={{ justifyContent: "center", alignItems: "center" }}>
+      <Image style={{ height: 25, width: 25 }} source={icon} />
+      <Text>{label}</Text>
+    </View>
+  );
 }
--- navigation/Tab.js
+++ navigation/Tab.js
@@ -1,29 +1,46 @@
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
-import React from "react"
-import { TabIcon } from "../components"
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import React from "react";
+import { TabIcon } from "../components";
 
 //Components
 import Artists from "../screens/Artists";
 import Home from "../screens/Home";
 
+//Icons
+import { icons } from "../constants";
+
 const Tab = createBottomTabNavigator();
 
 const Tabs = () => {
-    return (
-        <Tab.Navigator initialRouteName="Artists" tabBarOptions={{
-            showLabel: false
-        }}  >
-            <Tab.Screen name="Home" component={Home} options={{
-                tabBarIcon: ({ focused }) => {
-                    return <TabIcon focused={focused} label="Home" />
-                }
-            }} />
-            <Tab.Screen name="Artists" component={Artists} options={{
-                tabBarIcon: ({ focused }) => {
-                    return <TabIcon focused={focused} label="Artists" />
-                }
-            }} />
-        </Tab.Navigator>)
-}
+  return (
+    <Tab.Navigator
+      initialRouteName="Artists"
+      tabBarOptions={{
+        showLabel: false,
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ focused }) => {
+            return <TabIcon focused={focused} label="Home" icon={icons.home} />;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Artists"
+        component={Artists}
+        options={{
+          tabBarIcon: ({ focused }) => {
+            return (
+              <TabIcon focused={focused} label="Artists" icon={icons.info} />
+            );
+          },
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
 
-export default Tabs
+export default Tabs;
