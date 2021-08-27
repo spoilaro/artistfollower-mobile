914dc0e40ab9a1828445530936da0d58e49ab983
Lionbull <geo1529@yahoo.com>
2021-08-27 16:56:07 +0300

    Created 'wireframe' level design on profile page

--- screens/Profile.js
+++ screens/Profile.js
@@ -10,6 +10,7 @@ import {
 import axios from "axios";
 import ArtistButton from "../components/ArtistButton";
 import { colors } from "../constants";
+import SearchBar from "react-native-elements/dist/searchbar/SearchBar-ios";
 
 export default function Profile() {
   return (
@@ -19,7 +20,17 @@ export default function Profile() {
         height: "100%",
       }}
     >
-      <Text>Profile</Text>
+      <View style={{backgroundColor: "#4f4f4f", borderWidth: 1, borderRadius: 5}}>
+        <Text style={{ color: colors.white, fontSize: 25, padding: 10 }}>
+          Followed Artists
+        </Text>
+      </View>
+      <SearchBar></SearchBar>
+      <ArtistButton/>
+      <ArtistButton/>
+      <ArtistButton/>
     </View>
   );
 }
+
+const styles = StyleSheet.create({});
